61f41bf0b6c6ded5f6aeab30f24e824f0eee6503
Theo Ford <theoford@Theos-MBP.cust.communityfibre.co.uk>
2024-09-06 15:27:24 +0100

    add prismic preview

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,10 @@ import type { Metadata } from "next";
 import { Nunito, Nunito_Sans } from "next/font/google";
 import clsx from "clsx";
 import "./globals.css";
-import { createClient } from "@/prismicio";
+import { createClient, repositoryName } from "@/prismicio";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
+import { PrismicPreview } from "@prismicio/next";
 
 const nunito = Nunito({
   subsets: ["latin"],
@@ -44,6 +45,7 @@ export default function RootLayout({
         {children}
         <Footer />
         <div className="fixed bg-gradient-to-tr from-emerald-50 to-cyan-50 z-[-1] inset-0 opacity-50"></div>
+        <PrismicPreview repositoryName={repositoryName} />
       </body>
     </html>
   );
